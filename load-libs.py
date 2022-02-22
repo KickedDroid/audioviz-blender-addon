005b205f9e85f1ddf5726e9d9862e51226039997
Aidan M <asmaidan@outlook.com>
2022-02-21 21:48:58 -0600

    Create load-libs.py

---
+++ load-libs.py
@@ -0,0 +1,21 @@
+import importlib
+import subprocess
+import os
+import sys
+
+python_exe = os.path.join(sys.prefix, 'bin', 'python.exe')
+
+def getDeps():
+    subprocess.call([python_exe, "-m", "ensurepip"])
+    doesExist()
+    print("Dependencies Updated!")
+
+def doesExist():
+    if importlib.util.find_spec("soundfile") is None:
+        subprocess.call([python_exe, "-m", "pip", "install", "soundfile"])
+        print("Downloading Soundfile through pip...")
+    if importlib.util.find_spec("librosa") is None:
+        subprocess.call([python_exe, "-m", "pip", "install", "librosa"])
+        print("Downloading librosa through pip...")
+    else: 
+        print("Required imports already installed.")
